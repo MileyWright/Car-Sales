fbe3b8a4e2b7953db6c68741c05a99f1394dc468
Miley Wright <dishaliekwright@yahoo.com>
2019-11-21 15:33:12 -0500

    hooked adding Items together and working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+//7. import connect then create a mapStateToProps function
+import {connect} from 'react-redux';
+
+import {addItem, removeItem, total} from './actions';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
@@ -26,24 +30,36 @@ const App = (props) => {
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
+    props.removeItem(item);
+    props.total(-item.price);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    props.addItem(item);
+    props.total(item.price)
   };
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <AddedFeatures car={props.car} removeFeature={props.removeFeature} additionalFeatures={props.additionalFeatures}/>
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} buyItem= {buyItem}/>
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+//8. create the mapStateToProps function
+function mapStateToProps(state){
+  return{
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice
+  };
+}
+export default connect(mapStateToProps, {addItem, removeItem, total})(App);
